00afc1e87a36aff00d1b1cc692a7a2c3e4141d60
jassler <erifetim@gmail.com>
2021-05-30 12:36:03 +0200

    Generating reloc entries when compiling

--- compile.sh
+++ compile.sh
@@ -1,1 +1,2 @@
-java -jar sjc.jar src -t ia32 -T nsop -o boot && /usr/local/Cellar/qemu/5.2.0_1/bin/qemu-system-x86_64 -boot a -serial stdio -fda BOOT_FLP.IMG
+./objRelocEntriesGenerator.sh
+java -jar sjc.jar src -t ia32 -s 10M -T nsop -o boot -n && /usr/local/Cellar/qemu/6.0.0/bin/qemu-system-x86_64 -boot a -serial stdio -fda BOOT_FLP.IMG
