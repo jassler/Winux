a3f9f8aabdd5100320c603310325b513234c3996
jassler <erifetim@gmail.com>
2021-05-30 12:38:27 +0200

    parseInt added

--- src/os/utils/Math.java
+++ src/os/utils/Math.java
@@ -10,4 +10,36 @@ public class Math {
         return (a<=b) ? a : b;
     }
 
+    public static int parseInt(String s, int errVal) {
+        int result, ptr, c;
+        boolean negative;
+
+        if(s == null || s.length() == 0)
+            return errVal;
+
+        result = 0;
+        if(s.charAt(0) == '-') {
+            ptr = 1;
+            negative = true;
+        } else {
+            ptr = 0;
+            negative = false;
+        }
+
+        while(ptr < s.length()) {
+            c = s.charAt(ptr) - '0';
+            if(c >= 0 && c <= 9)
+                result = (result * 10) + c;
+            else
+                return errVal;
+
+            ptr++;
+        }
+
+
+        if(negative)
+            return -result;
+        return result;
+    }
+
 }
